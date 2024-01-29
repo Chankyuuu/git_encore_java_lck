1fe3ccc5b3591ae97e5178d74ae86573731fa81b
Chankyuuu <121791324+Chankyuuu@users.noreply.github.com>
2024-01-29 16:35:40 +0900

    Update CarMain.java

--- encore_java_oop_lck_pjt/src/CarMain.java
+++ encore_java_oop_lck_pjt/src/CarMain.java
@@ -4,7 +4,7 @@ public class CarMain {
 	public static void main(String[] args) {
 		Car car = new Car("가솔린") ;
 		
-		// car 객체 생성후 인스턴스 소유의 메서드 호출
+		/// car 객체 생성후 인스턴스 소유의 메서드 호출
 		car.engineInfo();
 	}
 }
